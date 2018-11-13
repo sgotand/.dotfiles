0be550428a4d248a8e9ee99caa216136164982d4
progrunner17 <g.sho1500@gmail.com>
2018-11-13 14:10:59 +0900

    small fix

--- init.sh
+++ init.sh
@@ -1,17 +1,24 @@
+#! /bin/sh
+
 set -eu
 
 RED="\e[31m"
 GREEN="\e[32m"
+YELLOW="\e[33m"
 RESET="\e[0m"
+DOTDIR=$HOME/.dotfiles
 
 echo $GREEN [[start apt update]] $RESET
 apt update
 
 echo $GREEN [[installing ansible]] $RESET
-sudo apt install ansible
+sudo apt install ansible git
+
+echo $GREEN [[clonning .dotfiels]] $RESET
+ansible localhost --connection=local -m git -a "repo=https://github.com/progrunner17/.dotfiles.git dest=${DOTDIR} update=no"
 
 
 echo $GREEN [[start ansible]] $RESET
-echo $RED !!!!!! please input your password !!!!! $RESET
-ansible-playbook playbook.yml --ask-become-pass
+echo $YELLOW !!!!!! please input your password !!!!! $RESET
+ansible-playbook ${DOTDIR}/playbook.yml --ask-become-pass
 
