e3a87f2db028af362fe670395b26d7fe384b3791
Shotaro Gotanda <g.sho1500@gmail.com>
2021-06-19 00:51:36 +0900

    update fzf install script

--- scripts/install_fzf.sh
+++ scripts/install_fzf.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
-version=0.22.0
-os=linux
+set -eux
+version=0.27.2
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     os=linux;;
+    Darwin*)    os=darwin;;
+#     CYGWIN*)    os=windows;;
+#     MINGW*)     os=windows;;
+    *)          os="UNKNOWN:${unameOut}"
+esac
 arch=amd64
-file=fzf-${version}-${os}_${arch}.tgz
-url=https://github.com/junegunn/fzf-bin/releases/download/${version}/${file}
+file=fzf-${version}-${os}_${arch}.tar.gz
+url=https://github.com/junegunn/fzf/releases/download/${version}/${file}
 
 curl -fsSL ${url} |sudo tar -C /usr/local/bin -xz
