88f3f9093f28130175887b9a17f92493238e1685
Shotaro Gotanda <g.sho1500@gmail.com>
2022-06-19 10:08:52 +0900

    Add zinit settings

---
+++ zsh/zinit.zsh
@@ -0,0 +1,27 @@
+ZINIT_LOAD=load
+
+# zinit ice wait lucid atload'_zsh_autosuggest_start'
+zinit ${ZINIT_LOAD} "zsh-users/zsh-autosuggestions"
+
+zinit ice wait lucid blockf
+zinit ${ZINIT_LOAD} "zsh-users/zsh-completions"
+zinit ice wait lucid blockf
+zinit ${ZINIT_LOAD} "zsh-users/zsh-syntax-highlighting"
+# ! suppress
+zinit ice wait"1" lucid
+zinit ${ZINIT_LOAD} "jonmosco/kube-ps1"
+
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/rsync/rsync.plugin.zsh
+# add aliases like rsync-copy
+# zinit snippet OMZ::plugins/rsync/rsync.plugin.zsh
+
+zinit wait lucid for \
+        OMZL::git.zsh \
+  atload"unalias grv" \
+        OMZP::git
+
+# zinit snippet PZT::modules/helper/init.zsh
+
+## ↓ fzf is used instead
+# zinit light zdharma-continuum/history-search-multi-word
+# zinit light zsh-users/zsh-history-substring-search
---
+++ zsh/zplug.zsh
@@ -0,0 +1,53 @@
+if [ -z "${ZPLUG_HOME}" ]; then
+    export ZPLUG_HOME=$HOME/.zplug
+fi
+
+if [ ! -e "${ZPLUG_HOME}" ]; then
+    git clone https://github.com/zplug/zplug $ZPLUG_HOME
+fi
+
+if [ -e "$ZPLUG_HOME/init.zsh" ]; then
+    echo '######### start zplug init.zsh source #########'
+    source $ZPLUG_HOME/init.zsh
+    echo '######### finish zplug init.zsh source #########'
+    zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+    zplug "zsh-users/zsh-completions"
+
+    zplug "zsh-users/zsh-history-substring-search"
+    if zplug check "zsh-users/zsh-history-substring-search"; then
+        bindkey '^[[A' history-substring-search-up
+        bindkey '^[[B' history-substring-search-down
+        bindkey -M emacs '^P' history-substring-search-up
+        bindkey -M emacs '^N' history-substring-search-down
+        bindkey -M vicmd 'k' history-substring-search-up
+        bindkey -M vicmd 'j' history-substring-search-down
+    fi
+
+    zplug "zsh-users/zsh-syntax-highlighting", defer:2
+    zplug "zsh-users/zsh-autosuggestions", defer:2 # 履歴を薄く表示
+    if [ -f ~/.fzf.zsh ]; then
+      zplug "kwhrtsk/docker-fzf-completion", defer:2
+    fi
+
+    zplug "jonmosco/kube-ps1", defer:2, use:"*.sh"
+
+    zplug "plugins/cargo", from:oh-my-zsh, defer:2
+    zplug "plugins/fd", from:oh-my-zsh, defer:2
+    zplug "plugins/ripgrep", from:oh-my-zsh, defer:2
+    zplug "plugins/ansible", from:oh-my-zsh, defer:2
+    zplug "plugins/systemd", from:oh-my-zsh, defer:2
+    zplug "plugins/sudo", from:oh-my-zsh, defer:2
+    zplug "plugins/rsync", from:oh-my-zsh, defer:2
+    zplug "plugins/git", from:oh-my-zsh, defer:2
+    zplug "plugins/history", from:oh-my-zsh, defer:2
+    zplug "plugins/colored-man-pages", from:oh-my-zsh
+
+    echo '######### zplug check ##########'
+    zplug check --verbose || zplug install
+    echo '######### zplug load ##########'
+    zplug load --verbose
+    echo '######### zplug list ##########'
+    zplug list
+
+# NOTE: if you encountered load duplication problem, do zplug update
+fi
